f0cc720de265ad25ede88692a81c688a8bcafc7a
AnnaKrasovskaya <annkarry93@gmail.com>
2025-06-24 15:27:36 +0300

    add all sober

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import "./style.scss";
+const burger = document.getElementById("burger");
+const menuMobile = document.getElementById("menuMobile");
+const close = document.getElementById("close-burger");
+const questions = document.querySelectorAll(".blockQuestion");
+if (questions) {
+  questions.forEach((question) => {
+    question.addEventListener("click", () => {
+      question.classList.toggle("active");
+    });
+  });
+}
+
+if (burger) {
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    menuMobile.classList.toggle("active");
+  });
+}
+if (close) {
+  close.addEventListener("click", () => {
+    burger.classList.remove("active");
+    menuMobile.classList.remove("active");
+  });
+}
